59bd69f27edeed7ba217535169a3f6e83c8aee5c
garaggang <reshetnikovdd2005@gmail.com>
2025-12-14 13:58:29 +0300

    Update App.java

--- src/main/java/com/example/stego/App.java
+++ src/main/java/com/example/stego/App.java
@@ -2,14 +2,9 @@ package com.example.stego;
 
 import javax.swing.SwingUtilities;
 
-/**
- * Точка входа GUI-приложения.
- */
+
 public class App {
-    /**
-     * Запуск Swing GUI.
-     * @param args аргументы командной строки
-     */
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new MainFrame().setVisible(true));
     }
